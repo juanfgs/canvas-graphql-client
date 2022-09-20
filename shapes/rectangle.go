246c54184c8eb2b490e66c3e51958b516deef9ec
Juan Francisco Giménez Silva <juanfgs@gmail.com>
2022-09-20 10:42:24 -0300

    added outline

--- shapes/rectangle.go
+++ shapes/rectangle.go
@@ -10,6 +10,9 @@ type Rectangle struct {
 }
 
 func (r Rectangle) GetPoint(x int, y int) string {
+	if r.isOutline(x, y) {
+		return r.Outline
+	}
 	if r.isInBounds(x, y) {
 		return r.Fill
 	}
@@ -22,5 +25,22 @@ func (r Rectangle) isInBounds(x int, y int) bool {
 			return true
 		}
 	}
+	return false
+}
+
+func (r Rectangle) isOutline(x int, y int) bool {
+	if x == r.X && y >= r.Y && y < r.Y+r.Height {
+		return true
+	}
+	if y == r.Y && x >= r.X && x < r.X+r.Width {
+		return true
+	}
+	if x == r.X+r.Width && y >= r.Y && y < r.Y+r.Height {
+		return true
+	}
+	if y == r.Y+r.Height && x >= r.X && x <= r.X+r.Width {
+		return true
+	}
+
 	return false
 }
